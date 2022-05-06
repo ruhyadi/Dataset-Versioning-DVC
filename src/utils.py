2e080f67221cd44599107eaca831f9453c0ac6c2
Didi Ruhyadi <ruhyadi.dr@gmail.com>
2022-05-06 21:34:55 +0700

    Automatic tags {tag_name}

--- src/utils.py
+++ src/utils.py
@@ -13,14 +13,14 @@ def checkout_repo_tags(repository_path: str, tag_name: str):
     os.system('dvc checkout')
 
 def push_repo_tags(repository_path: str, tag_name: str):
+    os.system('dvc add data')
+    os.system('dvc push')
+
     repo = git.Repo(repository_path)
     repo.git.add(all=True)
     repo.git.commit('-m', 'Automatic tags {tag_name}', author='ruhyadi.dr@gmail.com')
     repo.create_tag(tag_name, message=f'Automatic tags {tag_name}')
     repo.remotes.origin.push(tag_name)
-    # push to dvc remote registry
-    os.system('dvc add data')
-    os.system('dvc push')
 
 if __name__ == '__main__':
     url = '.'
